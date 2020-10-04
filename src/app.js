b5f41c68e3b756abf2193d92a80cc2c64c09ce63
Vanne <kvcj4@gmail.com>
2020-10-04 21:26:56 +0200

    JS added

--- src/app.js
+++ src/app.js
@@ -4,8 +4,17 @@ function showTemperature(response) {
   temperatureElem.innerHTML = Math.round(response.data.main.temp);
 }
 
+function search(event) {
+  event.prevenDefault();
+  let cityname = document.querySelector("#cityname");
+  console.log(cityname);
+}
+
 let apiKey = "c5d9545edd6e77c87e757baa297f0d90";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=c5d9545edd6e77c87e757baa297f0d90&units=metric";
 
 axios.get(apiUrl).then(showTemperature);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
