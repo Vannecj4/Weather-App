73e43c0bed2d9674f2077668c833c6e34c482784
Vanne <kvcj4@gmail.com>
2020-10-04 22:57:33 +0200

    add temperature of city

--- src/app.js
+++ src/app.js
@@ -4,17 +4,19 @@ function showTemperature(response) {
   temperatureElem.innerHTML = Math.round(response.data.main.temp);
 }
 
-function search(event) {
+function testname(event) {
   event.preventDefault();
   let cityname = document.querySelector("#cityname");
-  console.log(cityname);
+  console.log(cityname.value);
 }
 
-let apiKey = "c5d9545edd6e77c87e757baa297f0d90";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=c5d9545edd6e77c87e757baa297f0d90&units=metric";
+function search(city) {
+  let apiKey = "c5d9545edd6e77c87e757baa297f0d90";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
 
-axios.get(apiUrl).then(showTemperature);
+search("Amsterdam");
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
+form.addEventListener("submit", testname);
