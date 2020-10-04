64d1517a17af7e5cf7e550d4a389ccee321f960d
Vanne <kvcj4@gmail.com>
2020-10-04 17:22:17 +0200

    Added initial JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+function showTemperature(response) {
+  console.log(response.data);
+}
 
+let apiKey = "c5d9545edd6e77c87e757baa297f0d90";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=c5d9545edd6e77c87e757baa297f0d90&units=metric";
+
+axios.get(apiUrl).then(showTemperature);
