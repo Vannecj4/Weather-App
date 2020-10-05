5cbca5142e07d2e44e03eb023112e1a6574bdf24
Vanne <kvcj4@gmail.com>
2020-10-05 20:30:44 +0200

    added forecast

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function showTemperature(response) {
-  console.log(response.data);
   let temperatureElem = document.getElementById("units");
   let humidityElement = document.getElementById("humidity");
   let feelElement = document.getElementById("realfeel");
@@ -44,17 +43,24 @@ function showTemperature(response) {
   );
 }
 
-function testname(event) {
-  event.preventDefault();
-  let cityname = document.querySelector("#cityname");
-  search(cityname.value);
-  console.log(cityname.value);
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  console.log(response.data.list[0]);
 }
 
 function search(city) {
   let apiKey = "c5d9545edd6e77c87e757baa297f0d90";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function testname(event) {
+  event.preventDefault();
+  let cityname = document.querySelector("#cityname");
+  search(cityname.value);
 }
 
 let form = document.querySelector("#search-form");
